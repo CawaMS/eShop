ea0b034d258a68b60dea2cda190c653ae74b5e88
Catherine Wang <cawa@microsoft.com>
2023-03-31 10:32:55 -0700

    add REdis Connection

--- eShop/Program.cs
+++ eShop/Program.cs
@@ -35,7 +35,7 @@ builder.Services.AddStackExchangeRedisCache(options =>
 
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.IdleTimeout = TimeSpan.FromDays(14);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
